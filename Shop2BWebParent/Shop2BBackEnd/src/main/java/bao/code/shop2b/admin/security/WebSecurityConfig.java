e6cc6ca09b24d495def3252be2e30957e0f81622
games <games@thaibao>
2022-09-18 22:03:52 +0700

    Insert Authorzation in Sercurity for Product

--- Shop2BWebParent/Shop2BBackEnd/src/main/java/bao/code/shop2b/admin/security/WebSecurityConfig.java
+++ Shop2BWebParent/Shop2BBackEnd/src/main/java/bao/code/shop2b/admin/security/WebSecurityConfig.java
@@ -1,73 +1,79 @@
-package bao.code.shop2b.admin.security;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsPasswordService;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-/**
- * @author bao
- *
- */
-@SuppressWarnings("deprecation")
-@Configuration
-@EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	package bao.code.shop2b.admin.security;
 	
-	@Bean
-	public UserDetailsService userDetailsService() {
-		return new ShopUserDetailsService();
-	}
+	import org.springframework.context.annotation.Bean;
+	import org.springframework.context.annotation.Configuration;
+	import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+	import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+	import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+	import org.springframework.security.config.annotation.web.builders.WebSecurity;
+	import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+	import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+	import org.springframework.security.core.userdetails.UserDetailsPasswordService;
+	import org.springframework.security.core.userdetails.UserDetailsService;
+	import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+	import org.springframework.security.crypto.password.PasswordEncoder;
 	
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
-	
-	public DaoAuthenticationProvider authenticationProvider() {
-		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-		authProvider.setUserDetailsService(userDetailsService());
-		authProvider.setPasswordEncoder(passwordEncoder());
+	/**
+	 * @author bao
+	 *
+	 */
+	@SuppressWarnings("deprecation")
+	@Configuration
+	@EnableWebSecurity
+	public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		
-		return authProvider;
-	}
+		@Bean
+		public UserDetailsService userDetailsService() {
+			return new ShopUserDetailsService();
+		}
 		
-
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.authenticationProvider(authenticationProvider());
-	}
-
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().authorizeRequests()
-		.antMatchers("/users/**").hasAuthority("Admin")
-		.antMatchers("/categories/**","/brands/**").hasAnyAuthority("Admin","Editor")
-		.antMatchers("/products/**").hasAnyAuthority("Admin","Editor","Salesperson","Shipper")
-		.anyRequest().authenticated()
-		.and()
-		.formLogin()
-			.loginPage("/login")
-			.usernameParameter("email")
-			.permitAll()
-		.and().logout().permitAll()
-		.and()
-			.rememberMe()
-			.key("AbcDefgHijLmnOpqrst_123456789")
-			.tokenValiditySeconds(7*24*60*60);
-	}
-
-	@Override
-	public void configure(WebSecurity web) throws Exception {
-		web.ignoring().antMatchers("/images/**","/js/**","/webjars/**");
-	}
+		@Bean
+		public PasswordEncoder passwordEncoder() {
+			return new BCryptPasswordEncoder();
+		}
+		
+		public DaoAuthenticationProvider authenticationProvider() {
+			DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+			authProvider.setUserDetailsService(userDetailsService());
+			authProvider.setPasswordEncoder(passwordEncoder());
+			
+			return authProvider;
+		}
+			
 	
-}
+		@Override
+		protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+			auth.authenticationProvider(authenticationProvider());
+		}
+	
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.csrf().disable().authorizeRequests()
+			.antMatchers("/users/**").hasAuthority("Admin")
+			.antMatchers("/categories/**","/brands/**").hasAnyAuthority("Admin","Editor")
+			.antMatchers("/products","/products/",
+					"/products/detail/**","/products/page/**").hasAnyAuthority("Admin","Editor",
+					"Salesperson","Shipper")
+			.antMatchers("/products/new","/products/delete/**").hasAnyAuthority("Admin","Editor")
+			.antMatchers("/products/edit/**","/products/save",
+					"products/check_unique").hasAnyAuthority("Admin","Editor","Salesperson")
+			.antMatchers("/products/**").hasAnyAuthority("Admin","Editor")
+			.anyRequest().authenticated()
+			.and()
+			.formLogin()
+				.loginPage("/login")
+				.usernameParameter("email")
+				.permitAll()
+			.and().logout().permitAll()
+			.and()
+				.rememberMe()
+				.key("AbcDefgHijLmnOpqrst_123456789")
+				.tokenValiditySeconds(7*24*60*60);
+		}
+	
+		@Override
+		public void configure(WebSecurity web) throws Exception {
+			web.ignoring().antMatchers("/images/**","/js/**","/webjars/**");
+		}
+		
+	}
