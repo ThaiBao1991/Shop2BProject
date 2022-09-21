0f88d35dcc188e4736065547322977fe96a5d591
games <games@thaibao>
2022-09-21 09:58:54 +0700

    Code Listing Product in a Category

---
+++ Shop2BWebParent/Shop2BFrontEnd/src/main/java/bao/code/shop2b/product/ProductRepository.java
@@ -0,0 +1,15 @@
+package bao.code.shop2b.product;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import bao.code.shop2b.common.entity.Product;
+
+public interface ProductRepository extends PagingAndSortingRepository<Product, Integer> {
+	@Query("SELECT p FROM Product p WHERE p.enabled = true " + 
+			"AND (p.category.id = ?1 OR p.category.allParentIDs LIKE %?2%)" +
+			" ORDER BY p.name ASC")
+	public Page<Product> listByCategory(Integer categoryIdm,String categoryIDMatch,Pageable pageable);
+}
