01896060dd5a9167d031cc3284da016440498122
games <games@thaibao>
2022-09-26 21:55:18 +0700

    Add submenu Items for Setting menu

--- Shop2BWebParent/Shop2BBackEnd/src/main/resources/static/js/common.js
+++ Shop2BWebParent/Shop2BBackEnd/src/main/resources/static/js/common.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
     });
 
     customizeDropMenu();
+    customizeTabs();
 });
 
 function customizeDropMenu(){
@@ -19,6 +20,19 @@ function customizeDropMenu(){
     $(".dropdown >a").click(function(){
         location.href = this.href;
     });
+}
+
+function customizeTabs(){
+    //Javascript to enable link to tab
+    var url = document.location.toString();
+    if(url.match('#')){
+        $('.nav-tabs a[href="#' + url.split('#')[1] +'"]').tab('show');
+    }
+
+    //Change hash for page-reload
+    $('.nav-tabs a').on('shown.bs.tab',function(e){
+            window.location.hash = e.target.hash;
+    })
 }
 
 function showModalDialog(title, message) {
