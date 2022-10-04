16c48e62bcc43a100a3d23cdc6838104c8a9165b
games <games@thaibao>
2022-10-04 11:02:29 +0700

    Code Login with Facebook ID

--- Shop2BWebParent/Shop2BFrontEnd/src/main/java/bao/code/shop2b/security/oauth/OAuth2LoginSuccessHandler.java
+++ Shop2BWebParent/Shop2BFrontEnd/src/main/java/bao/code/shop2b/security/oauth/OAuth2LoginSuccessHandler.java
@@ -7,7 +7,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
@@ -28,17 +27,30 @@ public class OAuth2LoginSuccessHandler extends SavedRequestAwareAuthenticationSu
 		String name = oAuth2User.getName();
 		String email = oAuth2User.getEmail();
 		String countryCode = request.getLocale().getCountry();
+		String clientName = oAuth2User.getClientName();
 		
 		System.out.println("OAuth2LoginSuccessHanlder : "+ name + " | " + email);
+		System.out.println("Client Name : " +clientName);
+		
+		AuthenticationType authenticationType = getAuthenticationType(clientName);
 		
 		Customer customer = customerService.getCustomerByEmail(email);
 		if(customer == null) {
-			customerService.addNewCustomerUponOAuthLogin(name,email,countryCode);
+			customerService.addNewCustomerUponOAuthLogin(name,email,countryCode,authenticationType);
 		}else {
-			customerService.updateAuthentication(customer, AuthenticationType.GOOGLE);
+			customerService.updateAuthentication(customer, authenticationType);
 		}
 		
 		super.onAuthenticationSuccess(request, response, authentication);
 	}
-
+	
+	private AuthenticationType getAuthenticationType(String clientName) {
+		if(clientName.equals("Google")) {
+			return AuthenticationType.GOOGLE;
+		}else if( clientName.equals("Facebook")) {
+			return AuthenticationType.FACEBOOK;
+		}else {
+			return AuthenticationType.DATABASE;
+		}
+	}
 }
